469a575a0671cd6c131032b8fff6b2982c9e6782
batburger <a_modenbach@outlook.com>
2018-06-22 22:06:40 +0200

    Added implementation of Stack class

--- header/Stack.h
+++ header/Stack.h
@@ -5,9 +5,24 @@
 #ifndef VM2_0_STACK_H
 #define VM2_0_STACK_H
 
+#include <vector>
+#include <cstdint>
+
 namespace vm2{
     class Stack {
+    public:
+        Stack();
+        ~Stack();
 
+    private:
+        std::vector<uint32_t>* data;
+
+    public:
+        void push(uint32_t value);
+        uint32_t pop();
+
+        void write(size_t index, uint32_t value);
+        uint32_t read(size_t index);
     };
 }
 
--- source/Stack.cpp
+++ source/Stack.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "../header/Stack.h"
+
+vm2::Stack::Stack() {
+    data = new std::vector<uint32_t>();
+
+    data->reserve(1024 / 4);
+}
+
+vm2::Stack::~Stack() {
+    delete data;
+}
+
+void vm2::Stack::push(uint32_t value) {
+    data->push_back(value);
+}
+
+uint32_t vm2::Stack::pop() {
+    return data->at(data->size() - 1);
+}
+
+void vm2::Stack::write(size_t index, uint32_t value) {
+    data->at(index) = value;
+}
+
+uint32_t vm2::Stack::read(size_t index) {
+    return data->at(index);
+}
