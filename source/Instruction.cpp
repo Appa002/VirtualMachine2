107c915de3802988a6b3b19078534557da995269
batburger <a_modenbach@outlook.com>
2018-06-28 22:17:10 +0200

    Uses IInstruction

--- source/Instruction.cpp
+++ source/Instruction.cpp
@@ -12,12 +12,11 @@ vm2::Instruction::Instruction(void (*func)(vm2::State *)) {
     this->func = std::function<void(vm2::State*)>(func);
 }
 
-vm2::Instruction vm2::Instruction::operator()(vm2::State *state) {
+void vm2::Instruction::operator()(vm2::State *state) {
     this->func(state);
-    return *this;
 }
 
-vm2::Instruction vm2::Instruction::call(vm2::State* state) {
-    return this->operator()(state);
+void vm2::Instruction::call(vm2::State* state) {
+    this->operator()(state);
 }
 
