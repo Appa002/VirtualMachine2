22415cf777e3329d53d1fafec57eeb7af7d25b40
batburger <a_modenbach@outlook.com>
2018-06-28 22:15:37 +0200

    Made sure the Instruction pointer gets iterated.

--- source/SpecialInstructionImplementation.cpp
+++ source/SpecialInstructionImplementation.cpp
@@ -6,21 +6,29 @@
 
 vm2::ReadNthRegisterInstruction::ReadNthRegisterInstruction(int n) : n(n) {}
 
-vm2::Instruction vm2::ReadNthRegisterInstruction::operator()(vm2::State* state){
+void vm2::ReadNthRegisterInstruction::operator()(vm2::State* state){
     uint32_t storedValue = state->readRegister((unsigned)n);
     state->getStack().push(storedValue, (uint8_t)(0xa0 + n));
-    return *this;
+    state->iterateIp();
+}
+
+void vm2::ReadNthRegisterInstruction::call(vm2::State *state) {
+    this->operator()(state);
 }
 
 ///////////////////////////////////////////////////////
 
 vm2::SetNthRegisterInstruction::SetNthRegisterInstruction(int n) : n(n){}
 
-vm2::Instruction vm2::SetNthRegisterInstruction::operator()(vm2::State *state) {
+void vm2::SetNthRegisterInstruction::operator()(vm2::State *state) {
     StackObject obj = state->getStack().pop();
     if(!obj.isGood())
         throw std::runtime_error("Trying to write none good value to a register.");
 
     state->setRegister((uint32_t)n, obj.get());
-    return *this;
+    state->iterateIp();
+}
+
+void vm2::SetNthRegisterInstruction::call(vm2::State *state) {
+    this->operator()(state);
 }
