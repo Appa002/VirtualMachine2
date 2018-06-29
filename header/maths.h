a5eafb205fe5bac0cfec9950c04f825ae9ca6784
batburger <a_modenbach@outlook.com>
2018-06-29 22:42:39 +0200

    Initial commit

---
+++ header/maths.h
@@ -0,0 +1,15 @@
+//
+// Created by a_mod on 29.06.2018.
+//
+
+#ifndef VM2_0_MATHS_H
+#define VM2_0_MATHS_H
+
+#include <cstdint>
+
+namespace vm2::maths{
+    uint32_t manualSignedAdding(uint32_t a, uint32_t b);
+    uint32_t manualSignedSubtracting(uint32_t a, uint32_t b);
+}
+
+#endif //VM2_0_MATHS_H
---
+++ source/maths.cpp
@@ -0,0 +1,46 @@
+//
+// Created by a_mod on 29.06.2018.
+//
+
+#include <cstdint>
+#include "../header/maths.h"
+
+uint32_t vm2::maths::manualSignedAdding(uint32_t a, uint32_t b) {
+    bool isAPositive = (a & (uint32_t)1 << 31) == 0;
+    bool isBPositive = (b & (uint32_t)1 << 31) == 0;
+    bool sinageDiffers = isAPositive != isBPositive;
+
+    if(!sinageDiffers){
+        if(isAPositive)
+            return (a + b) & 0xffffffff >> 1;
+        else
+            return manualSignedSubtracting(a, b & 0xffffffff >> 1);
+    }else{
+        if(isAPositive)
+            return manualSignedSubtracting(a, b & 0xffffffff >> 1);
+        else
+            return manualSignedSubtracting(b, a & 0xffffffff >> 1);
+    }
+}
+
+uint32_t vm2::maths::manualSignedSubtracting(uint32_t a, uint32_t b) {
+    bool isAPositive = (a & (uint32_t)1 << 31) == 0;
+    bool isBPositive = (b & (uint32_t)1 << 31) == 0;
+    bool sinageDiffers = isAPositive != isBPositive;
+
+    if(!sinageDiffers){
+        if(!isAPositive)
+            return manualSignedAdding(a, b & 0xffffffff >> 1);
+        else{
+            if(a > b || a == b)
+                return (a - b) & 0xffffffff >> 1;
+            else if (a < b)
+                return (b - a) | (uint32_t)1 << 31;
+        }
+    }else{
+        if(!isAPositive)
+            return (a + b) | (uint32_t)1 << 31;
+        else
+            return (a + b) & 0xffffffff >> 1;
+    }
+}
