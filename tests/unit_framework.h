eb1f33235067a60f87670719ab281506b2795d30
batburger <a_modenbach@outlook.com>
2018-07-03 14:59:52 +0200

    Changed to use c++ containers.

--- tests/unit_framework.h
+++ tests/unit_framework.h
@@ -8,7 +8,7 @@
 /*
  * This file contains super crude code for unit testing.
  * The Only reason this exists is because I can't be bothered to learn to use an actual unit testing framework.
- * With "learn" I, of course, mean having to setup the library.
+ * With "learn" I mean having to setup the library.
 */
 
 /*
@@ -18,32 +18,26 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <vector>
+#include <string>
 
 typedef int(* test_t)();
 
-test_t* registered_tests = nullptr;
-char** registered_tests_names = nullptr;
-unsigned int num_of_test = 0;
+std::vector<test_t> registered_tests;
+std::vector<std::string> registered_tests_names;
 
 #define register_test(func){ \
-    if(registered_tests == 0) {\
-        registered_tests = (test_t*)malloc(sizeof(test_t) * 255); \
-        registered_tests_names = (char**)malloc(sizeof(char) * 255);\
-    }\
-    registered_tests[num_of_test] = func; \
-    registered_tests_names[num_of_test] = (char*)malloc(sizeof(char) * strlen(#func) + 1); \
-    strcpy(registered_tests_names[num_of_test], #func); \
-    num_of_test++; \
+    registered_tests.push_back(func);\
+    registered_tests_names.push_back(std::string(#func)); \
 }
 
 #define ASSERT_EQUAL(a, b) if(a!=b) return 1;
-#define ASSERT_EQUAL_STRING(str_a, str_b) if(strcmp(str_a, str_b) != 0) return 1;
 
 void start_unit_test(){
     int successful_tests = 0;
-    for(unsigned int i = 0; i < num_of_test; i++){
+    for(unsigned int i = 0; i < registered_tests.size(); i++){
         printf("Checking test ... \"");
-        printf(registered_tests_names[i]);
+        printf(registered_tests_names[i].c_str());
         printf("\"\n");
         int result = registered_tests[i]();
         printf("    -> %i ", result);
@@ -56,15 +50,10 @@ void start_unit_test(){
 
     }
     printf("==============\n");
-    printf("Ran: %i, Successful: %i, Failed: %i, %f%%\n", num_of_test, successful_tests, num_of_test - successful_tests, ((double)successful_tests / (double)num_of_test) * 100);
-}
-
-void end_unit_test(){
-    free(registered_tests);
-    for(unsigned int i = 0; i < num_of_test; i++){
-        free(registered_tests_names[i]);
-    }
-    free(registered_tests_names);
+    printf("Ran: %u, Successful: %u, Failed: %u, %f%%\n",
+           (unsigned)registered_tests.size(), successful_tests,
+           (unsigned)registered_tests.size() - successful_tests,
+           ((double)successful_tests / (double)registered_tests.size()) * 100);
 }
 
 
