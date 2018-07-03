071f1482826def43c04cb6563d6d8ec0bfcc9d8d
batburger <a_modenbach@outlook.com>
2018-07-03 15:08:07 +0200

    Initial Commit

---
+++ header/LinearMemory.h
@@ -0,0 +1,32 @@
+//
+// Created by a_mod on 03.07.2018.
+//
+
+#ifndef VM2_0_LINEARMEMORY_H
+#define VM2_0_LINEARMEMORY_H
+
+#include <cstdint>
+#include <vector>
+#include "Page.h"
+
+namespace vm2{
+    class LinearMemory {
+    public:
+        LinearMemory();
+        ~LinearMemory() = default;
+
+    private:
+        uint32_t pageSize;
+        std::vector<vm2::Page*>* pages;
+
+    public:
+        void setPageSize(uint32_t newSize);
+        uint32_t getPageSize();
+        void allocPage();
+        void write(uint32_t address, uint32_t value);
+        uint32_t read(uint32_t address);
+    };
+}
+
+
+#endif //VM2_0_LINEARMEMORY_H
---
+++ source/LinearMemory.cpp
@@ -0,0 +1,53 @@
+//
+// Created by a_mod on 03.07.2018.
+//
+
+#include <stdexcept>
+#include <cmath>
+#include "../header/LinearMemory.h"
+
+vm2::LinearMemory::LinearMemory() {
+    pageSize = 0;
+    pages = new std::vector<Page *>;
+}
+
+void vm2::LinearMemory::setPageSize(uint32_t newSize) {
+    if (this->pageSize != 0)
+        throw std::runtime_error("The size of linear memory pages may only be set one time!");
+    this->pageSize = newSize;
+}
+
+void vm2::LinearMemory::allocPage() {
+    if (this->pageSize == 0)
+        throw std::runtime_error("The size of linear memory pages has not been set.");
+
+    pages->push_back(new Page(this->pageSize));
+}
+
+void vm2::LinearMemory::write(uint32_t address, uint32_t value) {
+    if (address >= pages->size() * pageSize)
+        throw std::runtime_error("Write address out of range!");
+
+    double numOfPagesRequired = (double)address / pageSize;
+
+    auto pageIndex = static_cast<uint32_t>(floor(numOfPagesRequired));
+    uint32_t index = address - pageIndex * pageSize;
+    pages->at(pageIndex)->writeTo(index, value);
+}
+
+uint32_t vm2::LinearMemory::read(uint32_t address) {
+    if (address >= pages->size() * pageSize)
+        throw std::runtime_error("Write address out of range!");
+
+    double numOfPagesRequired = (double)address / pageSize;
+
+    auto pageIndex = static_cast<uint32_t>(floor(numOfPagesRequired));
+    uint32_t index = address - pageIndex * pageSize;
+    return pages->at(pageIndex)->readFrom(index);
+}
+
+uint32_t vm2::LinearMemory::getPageSize() {
+    return pageSize;
+}
+
+
